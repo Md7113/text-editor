3bbe61ee06084ca7c50672c568987c66b7541af0
Mason <davismasont@gmail.com>
2022-12-01 17:35:57 -0800

    add comments, initiate read me files

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # text-editor
+
+
+
+## Deployed link
+* [text editor](https://sivad-text-editor.herokuapp.com/)
+
+## site picture
+![site](site-img-jate.png)
+
+
+
+## Technologies Used
+- HTML : Used to format the content of the page
+- CSS : Used to style the HTML on the web browser
+- JavaScript : Used to give the page logical functions and a dynamic interface
+- express : Used to host the server locally
+- Heroku : Used to host the website
+- Git : Used to version control the code during the creation process
+- GitHub : hosting the repositroy responsible for the website
+
+## Authors
+- writen styled and built by Mason Davis
+
+## summary
+This repository contains all the code to generate a text editor, it uses webpack to cofnigure the contents and contains code to store content locally if the site goes offline. 
+
+## Code Snippet
+This is a snippet Showing the formula used to generate the Local Database and how it works.
+
+```javaScript
+     const initdb = async () =>
+  openDB("jate", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
+        return;
+      }
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
+    },
+  });
+```
+
+
+## Author links
+[LinkedIn](https://www.linkedin.com/in/davis-mason-t/)
+[GitHub](https://github.com/Md7113)
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,48 +1,40 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('Post to the database');
-
-  const contactDb = await openDB('jate', 1);
-  
- const tx = contactDb.transaction('jate', 'readwrite');
-
- const store = tx.objectStore('jate');
-
-  const request = store.add({ id: 1, value: content });
-
+  console.log("Post to the database");
+  const contactDb = await openDB("jate", 1);
+  const tx = contactDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  //this is not my solution this was an error me and a fellow student were having. 
+  //the solution came from an ASKBCS, this is why our answers to this prblem look very similar
+  const request = store.add({ id: 1, content: content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
-
+  console.log("data saved to the database", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('GET from the database');
-
-  const contactDb = await openDB('jate', 1);
-
-  const tx = contactDb.transaction('jate', 'readonly');
-
-   const store = tx.objectStore('jate');
-
+  console.log("GET from the database");
+  const contactDb = await openDB("jate", 1);
+  const tx = contactDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  //this is not my solution this was an error me and a fellow student were having. 
+  //the solution came from an ASKBCS, this is why our answers to this prblem look very similar
   const request = store.get(1);
-
   const result = await request;
-  
-  return result?.value
-}
+  return result?.content;
+};
 initdb();
